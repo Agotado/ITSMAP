039963626fd8e63e696e4db4968c2491b4e1ec52
andreas <andreas_larsen408@hotmail.com>
2016-09-15 23:54:07 +0200

    added color support + custom icon + minor cleanup

--- Assignment1/Assignment1/app/src/main/java/com/example/andreas/assignment1/ViewAcivity.java
+++ Assignment1/Assignment1/app/src/main/java/com/example/andreas/assignment1/ViewAcivity.java
@@ -18,8 +18,6 @@ public class ViewAcivity extends AppCompatActivity {
     static String savedId;
     static Boolean savedDevInfo = false;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,27 +95,5 @@ public class ViewAcivity extends AppCompatActivity {
         savedInstanceState.putBoolean("savedDevInfo",savedDevInfo);
 
         super.onSaveInstanceState(savedInstanceState);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view_acivity, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
